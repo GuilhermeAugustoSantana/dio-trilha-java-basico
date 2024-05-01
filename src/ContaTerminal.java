599ad0dc291f7d2bc38e872ad967c85ab1ec2aca
guilherme <guilherme09augustoadams@gamil.com>
2024-05-01 11:10:59 -0400

    feat: add contaTerminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Informe seu nome: ");
+        nomeCliente = scan.nextLine();
+
+        System.out.println("Infome o numero da sua conta: ");
+        numero = scan.nextInt();
+
+        System.out.println("Informe a agência: ");
+        scan.nextLine();
+        agencia = scan.nextLine();
+
+        System.out.println("Informe seu saldo: ");
+        saldo = scan.nextDouble();
+
+        System.out.println("Olá ".concat(nomeCliente)+", obrigado por criar uma conta em nosso banco, sua agência é ".concat(agencia)+", conta "+ numero +" e seu saldo "+saldo+" já está disponível para saque.");
+    }
+}
